893740a95c9c60620f128af77ef64a6f2bedc2dc
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-05-04 13:28:22 -0400

    Create IntegrationTest project for  ASP.NET Core MVC. Allow grabbing config values from environment variables.

---
+++ Okta.AspNetCore.Mvc.IntegrationTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Okta.AspNetCore.Mvc.IntegrationTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
