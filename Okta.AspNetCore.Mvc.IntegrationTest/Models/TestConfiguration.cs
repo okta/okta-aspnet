47a4f4c8a6e54651a18c978956f1b5a4a7cd03a8
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-05-07 11:38:05 -0400

    Add tests to ensure that we have a redirect when try to access to a protected endpoint without the token.

---
+++ Okta.AspNetCore.Mvc.IntegrationTest/Models/TestConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Okta.AspNetCore.Mvc.IntegrationTest.Models
+{
+    public class TestConfiguration
+    {
+        private  static IConfiguration _configuration;
+
+        public static IConfiguration GetConfiguration()
+        {
+            if(_configuration == null)
+            {
+                _configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+            }
+
+            return _configuration;
+        }
+    }
+}
