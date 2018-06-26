0c9f2b74e5899889734a2310b7379cd7e47a5e35
Nate Barbettini <nathanaelb@gmail.com>
2018-06-26 12:50:15 -0700

    Add token handler tests

---
+++ Okta.AspNet.Abstractions.Test/StrictTokenHandlerShould.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="StrictTokenHandlerShould.cs" company="Okta, Inc">
+// Copyright (c) 2018-present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Okta.AspNet.Abstractions.Test
+{
+    public class StrictTokenHandlerShould
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("bad")]
+        public void FailForBadToken(string badToken)
+        {
+            var fakeOktaWebOptions = new OktaWebOptions
+            {
+                ClientId = "fake",
+                OktaDomain = "example.okta.com",
+            };
+            var fakeIssuer = "example.okta.com";
+
+            var handler = new StrictTokenHandler();
+
+            Action action = () => handler.ValidateToken(
+                badToken,
+                new DefaultTokenValidationParameters(fakeOktaWebOptions, fakeIssuer),
+                out _);
+
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
