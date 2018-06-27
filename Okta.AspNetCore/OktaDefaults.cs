a03fff47ea169ae9fb0360384224bcee54bd264a
Nate Barbettini <nathanaelb@gmail.com>
2018-06-26 18:14:21 -0700

    Add default strings

---
+++ Okta.AspNetCore/OktaDefaults.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="OktaDefaults.cs" company="Okta, Inc">
+// Copyright (c) 2018-present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace Okta.AspNetCore
+{
+    public static class OktaDefaults
+    {
+        public const string MvcAuthenticationScheme = OpenIdConnectDefaults.AuthenticationScheme;
+
+        public const string ApiAuthenticationScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+}
