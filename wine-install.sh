72f2e82715e864e6865be96803a0ab3035d7fe01
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-09-05 19:31:47 -0400

    Modify wine script

--- wine-install.sh
+++ wine-install.sh
@@ -10,5 +10,6 @@ sudo apt-get install --install-recommends winehq-stable
 curl 'https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks' --output ./winetricks
 chmod +x ./winetricks
 WINEPREFIX=$HOME/winedotnet452
+WINEARCH=win32
 wineboot --init
 ./winetricks --unattended dotnet452 corefonts
