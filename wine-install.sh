09d14ac3c7b6b7e6942a0e9423b59c42f72d5b51
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-09-05 18:04:00 -0400

    Modify wine script

--- wine-install.sh
+++ wine-install.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 dpkg --add-architecture i386 
 wget -nc https://dl.winehq.org/wine-builds/Release.key
-apt-key add Release.key
-apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
-apt-get update
-apt-get install --install-recommends winehq-stable
+sudo apt-key add Release.key
+sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+sudo apt-get update
+sudo apt-get install --install-recommends winehq-stable
 curl 'https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks' --output ./winetricks
 chmod +x ./winetricks
 WINEPREFIX=$HOME/winedotnet452
