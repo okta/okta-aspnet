3de1cc22e46553db10107fbe1e7276120884c9c2
Laura Rodriguez <rdz.maria.laura@gmail.com>
2018-09-05 18:33:36 -0400

    Modify wine script

--- wine-install.sh
+++ wine-install.sh
@@ -5,6 +5,7 @@ wget -nc https://dl.winehq.org/wine-builds/Release.key
 sudo apt-key add Release.key
 sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
 sudo apt-get update
+sudo apt-get install cabextract
 sudo apt-get install --install-recommends winehq-stable
 curl 'https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks' --output ./winetricks
 chmod +x ./winetricks
