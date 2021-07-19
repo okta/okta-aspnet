36629efc5de5ad9694b7d3067e4c2455a930ee26
Andrii Zhegurov <andrii.zhegurov@okta.com>
2021-07-19 19:43:00 +0300

    OKTA-378564 ASP.NET MVC > Add strong name / signature to the Asp.Net library

--- Okta.AspNet/AssemblyInfo.cs
+++ Okta.AspNet/AssemblyInfo.cs
@@ -5,4 +5,8 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Okta.AspNet.Test")]
+[assembly: InternalsVisibleTo("Okta.AspNet.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c9a1a6c8fc5450"+
+                                                 "11447302c48e3e39053e73cbe2ee76690f123727e88fe304cc4847eebb97b02f423d8678022911"+
+                                                 "21ed2640d24c482e6ec1362923d804c7078f4ff43990f9dbbbb800f04d4025d43a2e045e85b8aa"+
+                                                 "00fe7968eda585a92c47fc9db044f2dec7056f3bbcd01439daf54be057ace51eddf42c1cb7e107"+
+                                                 "ea982abd")]
