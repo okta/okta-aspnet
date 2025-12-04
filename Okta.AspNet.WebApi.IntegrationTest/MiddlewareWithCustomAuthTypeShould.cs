11d4fe9ba305a44e517172943246893ef9749e63
Aniket <aniket@okta.com>
2025-12-04 13:33:05 +0530

    fix: Correct file header names in StyleCop comments

--- Okta.AspNet.WebApi.IntegrationTest/MiddlewareWithCustomAuthTypeShould.cs
+++ Okta.AspNet.WebApi.IntegrationTest/MiddlewareWithCustomAuthTypeShould.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="MiddlewareShould.cs" company="Okta, Inc">
+﻿// <copyright file="MiddlewareWithCustomAuthTypeShould.cs" company="Okta, Inc">
 // Copyright (c) 2018-present Okta, Inc. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 // </copyright>
